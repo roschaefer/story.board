04789557388ee1c4d93fc3f8b491457c301148aa
Robert Schaefer <robert.schaefer@student.hpi.de>
2016-05-12 17:19:07 +0200

    add overview of the report

--- app/controllers/text_components_controller.rb
+++ app/controllers/text_components_controller.rb
@@ -1,21 +1,25 @@
 class TextComponentsController < ApplicationController
- def new
- end
+  def index
+    @text_components = TextComponent.all
+  end
 
- def show
-   @text_component = TextComponent.find(params[:id])
- end
+  def new
+  end
 
- def create
-   @text_component = TextComponent.new(text_component_params)
-   @text_component.save
-   redirect_to @text_component
- end
+  def show
+    @text_component = TextComponent.find(params[:id])
+  end
+
+  def create
+    @text_component = TextComponent.new(text_component_params)
+    @text_component.save
+    redirect_to @text_component
+  end
 
 
- private
+  private
 
- def text_component_params
-   params.require(:text_component).permit(:heading, :introduction, :main_part, :closing)
- end
+  def text_component_params
+    params.require(:text_component).permit(:heading, :introduction, :main_part, :closing)
+  end
 end
