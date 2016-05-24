0c5d7c60034634d56e537242019cdd291a946ff5
Robert Schaefer <robert.schaefer@student.hpi.de>
2016-05-25 00:21:57 +0200

    create seed data for sensor types

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+
+
+
+attribute_hashes = [
+  { :property =>"Temperature", :unit => "°C" },
+  { :property =>"Atmospheric Pressure", :unit => "Bar" },
+  { :property =>"Motion", :unit => "0-10" },
+  { :property =>"Brightness", :unit => "0-10" },
+  { :property =>"Volume", :unit => "0-10" },
+  { :property =>"Humidity", :unit => "%" },
+  { :property =>"Button", :unit => "On/Off" },
+  { :property =>"Geophone", :unit => "?...?" },
+  { :property =>"Gas: Methan", :unit => "%" },
+  { :property =>"Gas: Alcohol", :unit => "%" },
+  { :property =>"Gas: Ozone", :unit => "%" },
+  { :property =>"Air quality", :unit => "?...?" },
+  { :property =>"Vibration", :unit => "0-10" },
+  { :property =>"Relative Humidity" , :unit => "%" },
+]
+
+attribute_hashes.each  { |attributes| SensorType.create attributes }
