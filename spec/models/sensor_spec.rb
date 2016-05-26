8abd5d3855cb11a38dcb9960ba54c144446a722a
Robert Schaefer <robert.schaefer@student.hpi.de>
2016-05-26 23:07:07 +0200

    red: spec for helper method

--- spec/models/sensor_spec.rb
+++ spec/models/sensor_spec.rb
@@ -19,4 +19,38 @@ describe Sensor, :type => :model do
       expect{sensor.destroy}.to change{Sensor::Reading.count}.from(1).to(0)
     end
   end
+
+
+  describe "#active_text_components" do
+    let (:sensor) { create(:sensor) }
+
+    it "returns empty list if there are no sensor readings" do
+      expect(sensor.active_text_components).to eq []
+    end
+
+    context "with a sensor reading" do
+      before do
+        create(:sensor_reading, :sensor => sensor, :calibrated_value => 47)
+      end
+
+
+      context "without arguments" do
+
+        it "considers text components for the last sensor reading" do
+          text_component = create(:text_component, :main_part => "Some Text")
+          create(:condition, :text_component => text_component,
+                 :sensor => sensor, :from => 10, :to => 100)
+          expect(sensor.active_text_components).to include(text_component)
+        end
+
+        it "considers no text component if the last sensor reading is not in the interval" do
+          text_component = create(:text_component, :main_part => "Some Text")
+          create(:condition, :text_component => text_component,
+                 :sensor => sensor, :from => 50, :to => 100)
+          expect(sensor.active_text_components).not_to include(text_component)
+        end
+
+      end
+    end
+  end
 end
