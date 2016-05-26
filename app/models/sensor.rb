f98719d5fc3fc84d143334f57218bf64fef2eee8
Robert Schaefer <robert.schaefer@student.hpi.de>
2016-05-26 16:28:48 +0200

    green: feature read about latest data

--- app/models/sensor.rb
+++ app/models/sensor.rb
@@ -1,6 +1,8 @@
 class Sensor < ActiveRecord::Base
   has_many :sensor_readings, :class_name => Sensor::Reading, :dependent => :destroy
   belongs_to :sensor_type
+  has_many :conditions
+  has_many :text_components, :through => :conditions
 
   validates :name, :presence => true, :uniqueness => true
   validates :sensor_type, :presence => true
