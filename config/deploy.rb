321efd7e9396eda48304e80d8d29514a3fa5b163
Robert Schaefer <robert.schaefer@student.hpi.de>
2016-05-30 01:40:13 +0200

    configure capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,13 @@
 # config valid only for current version of Capistrano
 lock '3.5.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
-
+set :application, 'story.board'
+set :repo_url, 'https://github.com/roschaefer/story.board.git'
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+set :deploy_to, '~/story.board'
 
 # Default value for :scm is :git
 # set :scm, :git
@@ -43,6 +42,16 @@ namespace :deploy do
       # within release_path do
       #   execute :rake, 'cache:clear'
       # end
+    end
+  end
+
+
+  after :deploy, :restart_daemon
+
+  desc "Restart Daemon"
+  task :restart_daemon do
+    on roles(:all)  do
+      execute "svc -h ~/service/story.board"
     end
   end
 
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-# server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
+server 'perseus.uberspace.de', user: 'vicari', roles: %w{app db web}
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
