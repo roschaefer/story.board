fe1135da745f841abfd789ba1524c4468879ede8
Robert Schaefer <robert.schaefer@student.hpi.de>
2016-06-09 01:05:48 +0200

    define feature: priorities

--- spec/models/text_component_spec.rb
+++ spec/models/text_component_spec.rb
@@ -4,4 +4,11 @@ describe TextComponent, type: :model do
   context 'without a report' do
     specify { expect(build(:text_component, report: nil)).not_to be_valid }
   end
+
+  describe '#priority' do
+    it 'defaults to :medium' do
+      text_component = build(:text_component)
+      expect(text_component.priority).to eq "medium"
+    end
+  end
 end
