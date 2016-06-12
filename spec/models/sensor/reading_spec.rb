86d599025be2a7768db742c0d7def63781a42435
Robert Schaefer <robert.schaefer@student.hpi.de>
2016-06-13 00:25:07 +0200

    require float values in sensor readings

--- spec/models/sensor/reading_spec.rb
+++ spec/models/sensor/reading_spec.rb
@@ -4,4 +4,13 @@ describe Sensor::Reading, type: :model do
   context 'without a correspondent sensor' do
     specify { expect(build(:sensor_reading, sensor: nil)).not_to be_valid }
   end
+
+  [:calibrated_value, :uncalibrated_value].each do |attribute|
+    describe attribute do
+      it "stores float values" do
+        reading = build(:sensor_reading, attribute => 0.5)
+        expect(reading.send(attribute)).to eq 0.5
+      end
+    end
+  end
 end
