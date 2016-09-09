f27055b401f9796271d52570d33bb3657962d6cc
Robert Schaefer <robert.schaefer@student.hpi.de>
2016-09-10 00:41:02 +0200

    add spec for actor#activate!

--- spec/models/actor_spec.rb
+++ spec/models/actor_spec.rb
@@ -2,8 +2,30 @@ require 'rails_helper'
 require 'support/shared_examples/database_unique_attribute'
 
 RSpec.describe Actor, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
   describe '#name' do
     it_behaves_like 'database unique attribute', :actor, name: 'Whatever'
+  end
+
+  describe '#activate!' do
+    let(:actor) { create(:actor) }
+    subject { actor.activate! }
+    it 'creates a command' do
+      expect { subject }.to change { Command.count }.from(0).to(1)
+    end
+
+    it 'created command belongs to actor' do
+      expect { subject }.to change { actor.commands.size }.from(0).to(1)
+    end
+
+    it 'created command is not yet executed' do
+      subject
+      expect(Command.last.executed).to be_falsy
+    end
+
+    it 'created command\'s value == :on' do
+      subject
+      expect(Command.last.value).to eq :on
+    end
   end
 end
