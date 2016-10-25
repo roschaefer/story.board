e886100bc0c614a934d97504e40ec2fb1d884775
Robert Schaefer <robert.schaefer@student.hpi.de>
2016-10-25 15:59:07 +0200

    display actuator name in Chain#index

--- spec/models/chain_spec.rb
+++ spec/models/chain_spec.rb
@@ -9,4 +9,19 @@ RSpec.describe Chain, type: :model do
   describe '#hashtag' do
     it_behaves_like 'database unique attribute', :chain, hashtag: '#blah'
   end
+
+  describe '#actuator_name' do
+    subject { chain.actuator_name }
+    context 'actuator missing' do
+      let(:chain) { create(:chain, actuator: nil) }
+      it { is_expected.to be_nil }
+    end
+
+    context 'actuator present' do
+      let(:actuator) { create(:actuator) }
+      let(:chain) { create(:chain, actuator: actuator) }
+      it { is_expected.to eq actuator.name }
+    end
+  end
+
 end
