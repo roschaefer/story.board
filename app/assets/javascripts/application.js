bfba9dc34fa849c7a02e22521da35fb1c4a59f9a
Jan Tackmann <hello@jantackmann.de>
2016-11-29 19:07:15 +0100

    fixes unformatted values in initial tooltip value

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,41 +22,35 @@
 jQuery(document).ready(function($) {
 
 
-
-	$(".slider-temperature").slider({
+	var sliderTemperature = $(".slider-temperature").slider({
 		formatter: function(value) {
 			return value + ' °C';
 		}	
 	});
+	sliderTemperature.slider('setValue',[33,66]);
 
-	$(".slider-minutes").slider({
+
+	var sliderMinutes = $(".slider-minutes").slider({
 		formatter: function(value) {
 			return value + ' min';
 		}	
 	});
+	sliderMinutes.slider('setValue',6);
 
 
-	$(".slider-time-range").slider({
+	var sliderTimeRange = $(".slider-time-range").slider({
 	    min: 0,
 		max: 1439,
 		range: true,
-		step: 15,
+		step: 5,
 		tooltip_split: true,
 		ticks_tooltip: true,
 		tooltip: 'always',
 		formatter: function(value) {
-			//idea to prevent displaying unformatted value
-			//showTip($(".slider-time-range"));
 			return minutesToTime(value);
 		}	
 	});
-
-	var showTipVar = false
-	function showTip(elem){
-		if( showTipVar ){ return; }
-		$(".slider-time-range").prev(".slider").find(".tooltip").show();
-		showTipVar = true;
-	}
+	sliderTimeRange.slider('setValue',[480,1020]);
 
 	function minutesToTime(value){
 		minutes = parseInt(value % 60, 10),
@@ -70,41 +64,6 @@ jQuery(document).ready(function($) {
 		return hours + ':' + minutes;
 	}
 
-	/*
-	$('#slider-time-range').slider().on('slide', slideTime).data('slider');
-
-	function slideTime(event, ui){
-	    var val0 = $("#slider-time-range").slider("values", 0), getAttribute
-	        val1 = $("#slider-time-range").slider("values", 1),
-	        minutes = parseInt(val0 % 60, 10),
-	        hours = parseInt(val0 / 60 % 24, 10),
-	        minutes1 = parseInt(val1 % 60, 10),
-	        hours1 = parseInt(val1 / 60 % 24, 10);
-	    startTime = getTime(hours, minutes);
-	    endTime = getTime(hours1, minutes1);
-	    $("#slider-time-range .tooltip-min").text(startTime + ' - ' + endTime);
-	}
-	function getTime(hours, minutes) {
-	    var time = null;
-	    minutes = minutes + "";
-	    if (hours < 12) {
-	        time = "AM";
-	    }
-	    else {
-	        time = "PM";
-	    }
-	    if (hours == 0) {
-	        hours = 12;
-	    }
-	    if (hours > 12) {
-	        hours = hours - 12;
-	    }
-	    if (minutes.length == 1) {
-	        minutes = "0" + minutes;
-	    }
-	    return hours + ":" + minutes + " " + time;
-	}
-	slideTime();		
-	*/
+	
 
 });
