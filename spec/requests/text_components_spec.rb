83539457674721a2234d578fc594328e9ad118bc
Robert Schaefer <robert.schaefer@student.hpi.de>
2017-02-04 16:14:30 +0100

    expose regression with request spec

--- spec/requests/text_components_spec.rb
+++ spec/requests/text_components_spec.rb
@@ -7,4 +7,19 @@ RSpec.describe "TextComponents", type: :request do
       expect(response).to have_http_status(200)
     end
   end
+
+  describe "POST /text_components" do
+    it 'renders validation errors' do
+      post '/text_components', params: { text_component: { heading: nil }}
+      expect(response.body).to include("Heading can't be blank")
+    end
+  end
+
+  describe "PATCH /text_components/:id" do
+    it 'renders validation errors' do
+      tc = create(:text_component)
+      patch "/text_components/#{tc.id}", params: { text_component: { heading: nil }}
+      expect(response.body).to include("Heading can't be blank")
+    end
+  end
 end
