d5aeee8cb6a04c08c0f328037f7c278fe38b6afe
Leif Gensert <leif@leif.io>
2017-03-12 18:13:42 +0100

    initialize default contexts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170312124125) do
+ActiveRecord::Schema.define(version: 20170312171020) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
