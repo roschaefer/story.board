f35c0f411244a03c8a700d8b811f7743df293573
Leif Gensert <leif@leif.io>
2017-03-14 17:01:59 +0100

    make it possible to create default topics

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,17 @@ if Report.count == 0
   Report.create(name: 'Kuh Bertha', start_date: Time.now)
 end
 
+# Create default topics:
+
+Topic.find_or_create_by(name: "milk_quantity")
+Topic.find_or_create_by(name: "milk_quality")
+Topic.find_or_create_by(name: "movement")
+Topic.find_or_create_by(name: "temperature")
+Topic.find_or_create_by(name: "intake")
+Topic.find_or_create_by(name: "birth")
+Topic.find_or_create_by(name: "calf")
+Topic.find_or_create_by(name: "noise")
+Topic.find_or_create_by(name: "health")
 
 attribute_hashes = [
   { property: 'Temperature',          unit: '°C'     },
