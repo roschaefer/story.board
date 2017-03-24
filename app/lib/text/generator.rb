4d281125fd2d2cb1efe09c0df4b120d14c9bf7e8
Leif Gensert <leif@leif.io>
2017-03-24 11:36:16 +0100

    fix bugs due to merging

--- app/lib/text/generator.rb
+++ app/lib/text/generator.rb
@@ -64,7 +64,7 @@ module Text
     end
 
     def components
-      @compontents ||= Text::Sorter.sort(@report.active_sensor_story_components, @opts)
+      @compontents ||= Text::Sorter.sort(@report.active_sensor_story_components(@opts), @opts)
     end
   end
 end
