db834427f97c752c213c14012452d06810d4061f
Robert Schaefer <robert.schaefer@student.hpi.de>
2017-05-01 23:26:41 +0200

    help out in the controller

--- app/controllers/chatfuel_controller.rb
+++ app/controllers/chatfuel_controller.rb
@@ -2,88 +2,63 @@ class ChatfuelController < ApplicationController
   def show
     chatbot_channel = Channel.chatbot
 
-    tc = Text::Sorter.sort(chatbot_channel.text_components, {}).first
+    @text_component = Text::Sorter.sort(chatbot_channel.text_components, {}).first
 
-    if tc
-      text = Text::Renderer.new(text_component: tc).render(:main_part)
-
-      qa = tc.question_answers
-
-      #Test API Request: http://localhost:3000/chatfuel/1
-      if qa.exists?(1)
-          sendJsonResponse(text, 1, qa.find(1).question)
-      else
-          sendJsonResponse(text)
-      end
-
+    if @text_component
+      @next_question_answer = @text_component.question_answers.first
+      render json: json_response
     else
-     render json: {}, status: 404
+      render json: {}, status: 404
     end
   end
 
-  def showQuestionAndAnswer
-      # request for qa for a specific text component
-      # send answer as json response to chatfuel
-      # if more questions and answers available, append button for next answer to json response
-
-      chatbot_channel = Channel.chatbot
-
-      tc = Text::Sorter.sort(chatbot_channel.text_components, {}).first
-
-      if tc
-          qaId = params[:id]
-
-          if tc.question_answers.exists?(qaId)
-              qa = tc.question_answers.find(qaId)
-
-              if tc.question_answers.exists?(qaId.to_i + 1)
-                  # send answer + button for next question
-                  sendJsonResponse(qa.answer, qaId.to_i + 1, tc.question_answers.find(qaId.to_i + 1).question)
-              else
-                  # send only answer for this question
-                  sendJsonResponse(qa.answer)
-              end
-          else
-              render json: {}, status: 404
-          end
-
-      else
-          render json: {}, status: 404
-      end
+  def answer_to_question
+    @text_component = TextComponent.includes(:question_answers).find(params[:text_component_id])
+    index = params[:index].to_i
+    @question_answer = @text_component.question_answers[index]
+    @next_question_answer = nil # TODO: implement
+    render json: json_response
   end
 
-  def sendJsonResponse(text, qaId = 0, questionText = '')
-      if qaId.to_i == 0
-          @response = {
-            messages: [
-              { text: text.first(640)} # Messages for Facebook Messenger can only be 640 characters long. Source: https://developers.facebook.com/docs/messenger-platform/send-api-reference#request
-            ]
-          }
-      else
-          # send json response with text + qa button
-          # todo: create url for next question from question id
-          @response = {
-              "messages": [
-                  {
-                    "attachment": {
-                      "type": "template",
-                      "payload": {
-                        "template_type": "button",
-                        "text": text.first(640),
-                        "buttons": [
-                          {
-                            "type": "web_url",
-                            "url": chatfuel_qa_url(params[:topic], qaId), #'test',  #generate url for next question
-                            "title": questionText.first(640) #question
-                          }
-                        ]
-                      }
-                    }
-                  }
-                ]
-          }
-      end
+  private
 
-      render json: @response
+  def json_response
+    if @text_component
+      text = Text::Renderer.new(text_component: @text_component).render(:main_part)
+      messages =  [
+        { text: text.first(640)} # Messages for Facebook Messenger can only be 640 characters long. Source: https://developers.facebook.com/docs/messenger-platform/send-api-reference#request
+      ]
+    end
+
+    if @question_answer
+      messages << {
+        text: @question_answer.answer
+      }
+    end
+
+    if @next_question_answer
+      messages <<  {
+        attachment: {
+          payload: {
+            template_type: "button",
+            text: "I have actually no idea how Chatfuel renders this part. Is it some text up front to the button?",
+            buttons: [
+              {
+                url: answer_to_question_url(text_component_id: @text_component, index: @text_components.question_answers.index(@next_question_answer)),
+                type: "json_plugin_url",
+                title: @next_question_answer.question
+              }
+          ]
+          },
+          type: "template"
+        }
+      }
+    end
+
+    # return this hash
+    {
+      messages: messages
+    }
+
   end
 end
