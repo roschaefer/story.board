18c7204a3769769140d4abff51b330700d52ec82
Matthias Böhm <matthi@bonn-boehms.de>
2017-05-23 21:58:21 +0200

    fixed tests - dont require user_signed_in in test environment

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -59,6 +59,7 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 
   config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :view
 
   config.extend ControllerMacros, :type => :controller
 
