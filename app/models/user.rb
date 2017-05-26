6c6f35514fa38e29845ac6fa7b70384a36e8b253
Robert Schaefer <robert.schaefer@student.hpi.de>
2017-05-26 16:30:27 +0200

    User#name must be given

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :name, uniqueness: true
+  validates :name, uniqueness: true, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,10 @@ require 'support/shared_examples/database_unique_attribute'
 RSpec.describe User, type: :model do
   describe '#email' do
     it_behaves_like 'database unique attribute', :user, email: 'blablabla@example.org'
+    describe 'without name' do
+      subject { build(:user, name: '   ') }
+      it { is_expected.not_to be_valid }
+    end
   end
   describe '#name' do
     it_behaves_like 'database unique attribute', :user, name: 'XYZ'
