75984240640d7421c5b9fcf57238bb83e3e84fd0
Robert Schaefer <robert.schaefer@student.hpi.de>
2017-05-26 16:17:09 +0200

    user email and name are truly unique

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
+require 'support/shared_examples/database_unique_attribute'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#email' do
+    it_behaves_like 'database unique attribute', :user, email: 'blablabla@example.org'
+  end
+  describe '#name' do
+    it_behaves_like 'database unique attribute', :user, name: 'XYZ'
+  end
 end
