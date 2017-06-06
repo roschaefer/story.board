63bfbe4be1e1203748fbb1c0dc4ba93af36edef7
Matthias Böhm <matthi@bonn-boehms.de>
2017-06-06 20:33:28 +0200

    removed api test until it is clear how the smaxtec api login credentials are stored

--- spec/requests/smaxtec_api_spec.rb
+++ spec/requests/smaxtec_api_spec.rb
@@ -5,12 +5,6 @@ RSpec.describe 'Smaxtec API', type: :request do
   describe 'GET' do
     let(:url) { '/smaxtec_api/temperature' }
 
-    it 'reads temperature from Test API' do
-      get url
-      expect(response).to have_http_status(200)
 
-      json_response = JSON.parse(response.body)
-      expect(json_response.keys).to contain_exactly('temperature')
-    end
   end
 end
