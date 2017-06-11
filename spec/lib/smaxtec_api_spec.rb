b2ed2c827a28c31181fac4cfc9a5b3f855df0f87
Matthias Böhm <matthi@bonn-boehms.de>
2017-06-11 16:34:43 +0200

    added rspec test

---
+++ spec/lib/smaxtec_api_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+require 'vcr'
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'fixtures/vcr_cassettes'
+  config.hook_into :webmock
+end
+
+RSpec.describe SmaxtecApi do
+    let(:smaxtec_api) { SmaxtecApi.new() }
+
+  describe '.get_temeperature' do
+    it 'should read the current temperature of a specified animal from the Smaxtec API' do
+      VCR.use_cassette('smaxtec_api', :record => :new_episodes) do
+        expect(smaxtec_api.get_temperature()).to be > 0
+      end
+    end
+  end
+end
