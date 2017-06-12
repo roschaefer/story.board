8aa2190434c9e73ce8085aa5eedd921b0229e196
Matthias Böhm <matthi@bonn-boehms.de>
2017-06-12 19:21:37 +0200

    fixed test

--- spec/lib/smaxtec_api_spec.rb
+++ spec/lib/smaxtec_api_spec.rb
@@ -4,12 +4,16 @@ require 'vcr'
 VCR.configure do |config|
   config.cassette_library_dir = 'fixtures/vcr_cassettes'
   config.hook_into :webmock
+  config.default_cassette_options = {
+    :match_requests_on => [:method,
+      VCR.request_matchers.uri_without_param(:from_date, :to_date)]
+  }
 end
 
 RSpec.describe SmaxtecApi do
     let(:smaxtec_api) { SmaxtecApi.new() }
 
-  describe '.get_temeperature' do
+  describe '.get_temperature' do
     it 'should read the current temperature of a specified animal from the Smaxtec API' do
       VCR.use_cassette('smaxtec_api', :record => :new_episodes) do
         expect(smaxtec_api.get_temperature()).to be_kind_of(Float)
