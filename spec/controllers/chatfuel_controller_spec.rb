f136a36f9987db865f316f81888e3481d2e69070
Till Prochaska <mail@tillprochaska.de>
2017-06-13 23:42:36 +0200

    Extend chatfuel controller spec to test for markup replacement

--- spec/controllers/chatfuel_controller_spec.rb
+++ spec/controllers/chatfuel_controller_spec.rb
@@ -25,24 +25,35 @@ RSpec.describe ChatfuelController, type: :controller do
       end
 
       context "matching text component" do
-        let(:text_component) do
-          create(
-            :text_component,
-            report: report,
-            topic: topic,
-            channels: [chatbot_channel],
-            main_part: "The main part"
-          )
+
+        context "without markup" do
+          before { create(:text_component, report: report, topic: topic, channels: [chatbot_channel], main_part: "The main part") }
+
+          it { is_expected.to have_http_status(:ok) }
+
+          it "returns expected text" do
+            json_response = JSON.parse(subject.body)
+            expect(json_response["messages"].first["text"]).to eq("The main part")
+          end
         end
 
-        before { text_component }
+        context "with markup and sensory data" do
+          let(:sensor)        { create(:sensor, name: 'SensorXY', sensor_type: sensor_type) }
+          let(:sensor_type)   { create(:sensor_type, property: 'Temperature', unit: '°C') }
 
-        it { is_expected.to have_http_status(:ok) }
+          before do
+            create(:text_component, report: report, topic: topic, channels: [chatbot_channel], main_part: "SensorXY: { value(#{sensor.id}) }")
+            create(:sensor_reading, sensor: sensor, calibrated_value: 5)
+          end
 
-        it "returns expected text" do
-          json_response = JSON.parse(subject.body)
-          expect(json_response["messages"].first["text"]).to eq("The main part")
+          it { is_expected.to have_http_status(:ok) }
+
+          it "returns text with replaced markup" do
+            json_response = JSON.parse(subject.body)
+            expect(json_response["messages"].first["text"]).to eq("SensorXY: 5.0°C")
+          end
         end
+
       end
     end
   end
