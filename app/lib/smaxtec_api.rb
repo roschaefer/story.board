2b02126b4958110b557c074d08c2a300bc13219e
Matthias Böhm <matthi@bonn-boehms.de>
2017-06-16 15:29:32 +0200

    create sensor readings for temperature sensor

--- app/lib/smaxtec_api.rb
+++ app/lib/smaxtec_api.rb
@@ -6,23 +6,27 @@ class SmaxtecApi
   SMAXTEC_API_BASE_URL = 'https://api-staging.smaxtec.com/api/v1'
 
   def get_sensor_readings
-
-    #define animal_id for Cows: topic_id => animal_id
-    animal_ids = { 1 => '5722099ea80a5f54c631513d' }
-
-    # Get Temperature for Cows
+    #define animal_id (smaxtec api identifier) for cows: topic_id => animal_id
+    animal_id = { 1 => '5722099ea80a5f54c631513d' }
 
     # Assuming 1 report = 1 cow, otherwise get just the specified reports
     Report.all.each do |report|
-      if animal_ids[report.id]
+      if animal_id[report.id]
         sensors = Sensor.where(report_id: report.id, smaxtec_sensor: true)
 
         if sensors
-          binding.pry
           sensors.each do |sensor|
             # temperature sensor
-            if sensor.sensor_type == 1
-                #get temperature data & write to sensor readings
+            if sensor.sensor_type_id == 1
+              # get temperature data from smaxtec api
+              temperature = get_temperature(animal_id[report.id])
+
+              if temperature
+                # create new sensor readig for sensor
+                # compare temperature with latest sensor reading and only create new sensor reading if value has changed?
+                sensor_reading = Sensor::Reading.new(sensor_id: sensor.id, calibrated_value: temperature, uncalibrated_value: temperature)
+                sensor_reading.save!
+              end
             end
           end
         end
@@ -31,20 +35,17 @@ class SmaxtecApi
 
   end
 
+
   def get_temperature(animal_id)
     jwt_request = get_jwt
 
     if jwt_request
       @jwt = JSON.parse(jwt_request)['token']
-      organisation_id ='5721e3f8a80a5f54c6315131';
       #animal_id = '5722099ea80a5f54c631513d' # name = Arabella
       metric = 'temp'
-      unit = 'degree_celsius'
       temp_data = send_api_request('/data/query', { :animal_id => animal_id, :metric => metric, :from_date => Time.now.to_i - 3600, :to_date => Time.now.to_i, :aggregation => 'hourly.mean' })
 
       if temp_data && temp_data['data'].count() > 1
-        #sensor_reading = Sensor::Reading.new(sensor_id: 4, calibrated_value: temp_data['data'].last[1], uncalibrated_value: temp_data['data'].last[1])
-        #sensor_reading.save
         return temp_data['data'].last[1]
       else
         return false
@@ -54,6 +55,7 @@ class SmaxtecApi
       return false
     end
   end
+
 
   def get_jwt
     uri = URI(SMAXTEC_API_BASE_URL + '/user/get_token')
@@ -68,6 +70,7 @@ class SmaxtecApi
 
     return response.body
   end
+
 
   def send_api_request(url, params=nil)
     uri = URI(SMAXTEC_API_BASE_URL + url)
