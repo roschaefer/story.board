9669e92e0d05b659cfc8c402bb360c34d54e8061
Matthias Böhm <matthi@bonn-boehms.de>
2017-07-27 19:20:39 +0200

    allow blank animal ids for sensors

--- app/models/sensor.rb
+++ app/models/sensor.rb
@@ -15,7 +15,7 @@ class Sensor < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
   validates :address, presence: true, uniqueness: true
   validates :sensor_type, presence: true
-  validates :animal_id, uniqueness: { scope: :sensor_type }
+  validates :animal_id, uniqueness: { scope: :sensor_type }, :allow_blank => true
 
   def name_and_id
     "#{name} (#{id})"
