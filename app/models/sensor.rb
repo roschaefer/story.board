16ff7d1a636b8c226274fe2bfae2a321e386eca7
Matthias Böhm <matthi@bonn-boehms.de>
2017-07-27 18:52:41 +0200

    removed presence validation for sensor address field

--- app/models/sensor.rb
+++ app/models/sensor.rb
@@ -13,7 +13,7 @@ class Sensor < ActiveRecord::Base
 
   validates :report, presence: true
   validates :name, presence: true, uniqueness: true
-  validates :address, presence: true, uniqueness: true
+  validates :address, uniqueness: true
   validates :sensor_type, presence: true
   validates :animal_id, uniqueness: { scope: :sensor_type }
 
