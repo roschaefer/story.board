1a1905153948fc2f778f0cbb37f3f84b2ef48d93
Robert Schaefer <robert.schaefer@student.hpi.de>
2017-07-31 03:00:19 +0200

    implement test event/event_activation logic

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,5 @@
 class Event < ActiveRecord::Base
+  has_many :activations, class_name: 'Event::Activation'
   has_and_belongs_to_many :triggers
   validates :name, presence: true, uniqueness: true
 
@@ -8,5 +9,35 @@ class Event < ActiveRecord::Base
 
   def name_and_id
     "#{name} (#{id})"
+  end
+
+  def start(timestamp = nil)
+    given_timestamp = timestamp || DateTime.now
+    unless self.active?
+      Event::Activation.create!(event: self, started_at: given_timestamp)
+      true
+    else
+      false
+    end
+  end
+
+  def last_activation
+    self.activations.order(:started_at).last
+  end
+
+  def stop(timestamp = nil)
+    if self.active?
+      given_timestamp = timestamp || DateTime.now
+      la = self.last_activation
+      la.ended_at = given_timestamp
+      la.save!
+      true
+    else
+      false
+    end
+  end
+
+  def active?
+    self.last_activation && self.last_activation.ended_at.nil?
   end
 end
--- app/models/event/activation.rb
+++ app/models/event/activation.rb
@@ -1,3 +1,31 @@
 class Event::Activation < ApplicationRecord
   belongs_to :event
+
+  validates :started_at, presence: true
+  validate :ends_after_start
+  validate :no_overlapping
+
+
+  def ends_after_start
+    if self.ended_at && self.started_at && (self.ended_at < self.started_at)
+      errors.add(:ended_at, "can't end before started")
+    end
+  end
+
+  def no_overlapping
+    if self.started_at
+      overlapping_activation = Event::Activation.where.not(id: self.id).where('ended_at > ?', self.started_at)
+      if self.ended_at
+        overlapping_activation = overlapping_activation.where('started_at < ?', self.ended_at)
+      end
+      unless overlapping_activation.empty?
+        errors.add(:started_at, "time span overlaps with another activation")
+        errors.add(:ended_at, "time span overlaps with another activation")
+      end
+    end
+  end
+
+  def duration
+    self.ended_at && self.ended_at - self.started_at
+  end
 end
