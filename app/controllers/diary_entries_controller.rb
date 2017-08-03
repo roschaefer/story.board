d045c1c75886f5564178cfa786777207a9c9f530
Robert Schaefer <robert.schaefer@student.hpi.de>
2017-08-03 15:43:25 +0200

    refactor diary entries to use a common filter concern

--- app/controllers/diary_entries_controller.rb
+++ app/controllers/diary_entries_controller.rb
@@ -1,31 +1,20 @@
 class DiaryEntriesController < ApplicationController
   before_action :set_diary_entry, only: [:show,]
-  before_action :filter_diary_entries, only: [:index,]
+  include CommonFilters
 
   def index
+    @diary_entries = DiaryEntry.where(report: @report).order(:moment)
+    @diary_entries = filter_release(@diary_entries, filter_params)
+    @diary_entries = filter_timestamp(@diary_entries, filter_params, 'moment')
+    @diary_entries
   end
 
   def show
   end
 
   private
-  def diary_entries
-    DiaryEntry.where(report: @report).order(:moment)
-  end
-
   def set_diary_entry
     @diary_entry = DiaryEntry.find(params[:id])
-  end
-
-  def filter_diary_entries
-    @diary_entries = diary_entries
-    if filter_params[:release]
-      @diary_entries = @diary_entries.where(release: filter_params[:release])
-    end
-    if filter_params[:from] && filter_params[:to]
-      @diary_entries = @diary_entries.where('moment > ?',  filter_params[:from])
-      @diary_entries = @diary_entries.where('moment < ?',  filter_params[:to])
-    end
   end
 
   def filter_params
--- spec/requests/diary_entries_spec.rb
+++ spec/requests/diary_entries_spec.rb
@@ -1,11 +1,57 @@
 require 'rails_helper'
 
 RSpec.describe "DiaryEntries", type: :request do
-  describe "GET /reports/:id/diary_entries" do
-    it "works! (now write some real specs)" do
-      report = Report.current
-      get report_diary_entries_path(report)
-      expect(response).to have_http_status(200)
+  let(:report) { create(:report) }
+  let(:headers) { { 'ACCEPT' => 'application/json', 'Content-Type' => "application/json" } }
+  let(:params) { {} }
+  let(:js) { JSON.parse(response.body) }
+  describe "GET" do
+    let(:action) { get url, params: params, headers: headers }
+    describe "/reports/:id/diary_entries" do
+      let(:url) { "/reports/#{report.id}/diary_entries"}
+
+      let(:diary_entries) do
+        create(:diary_entry, id: 1, report: report, moment: '2017-07-16T12:19:00.000Z', release: 'final')
+        create(:diary_entry, id: 2, report: report, moment: '2017-07-17T12:19:00.000Z', release: 'debug')
+        create(:diary_entry, id: 3, report: report, moment: '2017-07-18T12:19:00.000Z', release: 'final')
+      end
+
+      describe 'filter' do
+        before do
+          diary_entries
+          action
+        end
+
+        describe 'for #moment' do
+          context 'params: { to: "2017-07-17T12:19:00.000Z" }' do
+            let(:params) { { to: "2017-07-17T12:19:00.000Z" } }
+            it 'contains only diary entries 1 and 2' do
+              expect(js[0]['id']).to eq 1
+              expect(js[1]['id']).to eq 2
+              expect(js.count).to eq 2
+            end
+          end
+
+          context 'params: { from: "2017-07-17T12:19:00.000Z" }' do
+            let(:params) { { from: "2017-07-17T12:19:00.000Z" } }
+            it 'contains only diary entries 2 and 3' do
+              expect(js[0]['id']).to eq 2
+              expect(js[1]['id']).to eq 3
+              expect(js.count).to eq 2
+            end
+          end
+        end
+
+        describe 'for #release' do
+          context 'params: { filter: {release: "debug" }' do
+            let(:params) { { release: "debug" } }
+            it 'contains only diary entries 2 and 3' do
+              expect(js[0]['id']).to eq 2
+              expect(js.count).to eq 1
+            end
+          end
+        end
+      end
     end
   end
 end
