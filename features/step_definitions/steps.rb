52683feb05ad4dca30dc307b7de7c70b80eb1824
Robert Schaefer <robert.schaefer@student.hpi.de>
2017-08-03 17:41:35 +0200

    change tests to match route nesting, remove unnecessary tests

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -673,7 +673,7 @@ When(/^all subsequent sensor readings will be intercepted for a while$/) do
       "calibrated_value": value,
       "uncalibrated_value": value,
     }.to_json
-    request '/sensor_readings', { method: :post, input: input }
+    request report_sensor_readings_path(Report.current, @sensor), { method: :post, input: input }
   end
   expect(@sensor.sensor_readings).to be_empty
 end
@@ -1334,5 +1334,10 @@ Given(/^we have these sensor readings for sensor (\d+) in our database:$/) do |s
            release: row['Release']
           )
   end
+end
+
+
+When(/^notice that we OVERRIDE the given sensor id (\d+) here$/) do |arg1|
+  # just documentation
 end
 
