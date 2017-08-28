d6e3196557d2256339b4ba8e660ecf536dcf8847
Robert Schaefer <robert.schaefer@student.hpi.de>
2017-08-28 23:08:43 +0200

    close #612, needs manual intervention on the production server

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # Use this file to easily define all of your cron jobs.
 #
 # It's helpful, but not entirely necessary to understand cron before proceeding.
@@ -6,14 +8,16 @@
 # Example:
 #
 set :output, './log/cron.log'
-#
- every 4.hours do
-   rake "archive"
- end
 
- every 15.minutes do
-   rake "smaxtec_api:update_sensors"
- end
+%w[6:05 9:05 13:05 18:05 23:05 3:05].each do |time|
+  every :day, at: time do
+    rake 'archive'
+  end
+end
+
+every 15.minutes do
+  rake 'smaxtec_api:update_sensors'
+end
 #
 # every 4.days do
 #   runner "AnotherModel.prune_old_records"
