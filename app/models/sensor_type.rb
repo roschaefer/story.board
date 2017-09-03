e04f28e63cccff4d54825277fef351e0326d1d80
Till Prochaska <mail@tillprochaska.de>
2017-09-03 21:07:08 +0200

    Add data collection property to sensor type model

--- app/models/sensor_type.rb
+++ app/models/sensor_type.rb
@@ -1,4 +1,5 @@
 class SensorType < ActiveRecord::Base
   default_scope { order(:property) }
   validates :unit, length: {minimum:  0, allow_nil: false}
+  enum data_collection_method: {automatic: 0, manual: 1}
 end
