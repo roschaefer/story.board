89b71106b2e5230e85a2be04217d518d132d6499
Robert Schaefer <robert.schaefer@student.hpi.de>
2017-09-11 21:03:12 +0200

    add one hour to all diary entries at 23:00

--- db/migrate/20170911185434_update_timeframes.rb
+++ db/migrate/20170911185434_update_timeframes.rb
@@ -4,5 +4,11 @@ class UpdateTimeframes < ActiveRecord::Migration[5.0]
       TextComponent.where(from_hour: 18, to_hour: 23).update_all(to_hour: 0)
       TextComponent.where(from_hour: 23, to_hour: 6).update_all(from_hour: 0)
     end
+    DiaryEntry.find_each do |entry|
+      if entry.moment.hour == 23
+        entry.moment += 1.hour
+        entry.save!
+      end
+    end
   end
 end
