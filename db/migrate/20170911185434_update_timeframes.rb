6def600e9c25dd64a03d4b2f02c1ce0e8c60f8c8
Robert Schaefer <robert.schaefer@student.hpi.de>
2017-09-11 20:55:10 +0200

    rake task => migration

--- db/migrate/20170911185434_update_timeframes.rb
+++ db/migrate/20170911185434_update_timeframes.rb
@@ -1,6 +1,5 @@
-namespace :sensorstory do
-  desc "Update text_component timeframes (from and to hour)"
-  task :update_timeframes => :environment do
+class UpdateTimeframes < ActiveRecord::Migration[5.0]
+  def change
     TextComponent.transaction do
       TextComponent.where(from_hour: 18, to_hour: 23).update_all(to_hour: 0)
       TextComponent.where(from_hour: 23, to_hour: 6).update_all(from_hour: 0)
